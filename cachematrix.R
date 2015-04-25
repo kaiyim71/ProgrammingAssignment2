70c20dac2e636db9a9bb0dffd8a8d1eda04de48e
kaiyim71 <kaiyim71@gmail.com>
2015-04-25 12:17:02 +0800

    my First Commit to R programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,41 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+## m is inverse of the matrix x, 
+## setmatrix  getmatrix will set/retrieve the inverse of matrix object x 
 
+makeCacheMatrix <- function(x = matrix()) {
+        m <- NULL      
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setmatrix <- function(matrix) m <<- matrix
+        getmatrix <- function() m
+        list(set = set, get = get,
+             setmatrix = setmatrix,
+             getmatrix = getmatrix)
 }
 
 
 ## Write a short comment describing this function
 
+## Return a matrix that is the inverse of 'x'
+##   First check whether we have a cached inverse matrix m for matrix x
+##   return if found
+##   If not found, create the object x in global env and cached the inverse of it
+##   return it 
+##
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getmatrix()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data)
+        x$setmatrix(m)
+        m        
+
 }
